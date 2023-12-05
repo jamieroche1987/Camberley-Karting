88eeae7a1ee722694cdf8cbb57f7a575098fbf61
jamieroche1987 <>
2023-12-05 21:51:59 +0000

    Edit Static_root

--- camberleykarting/settings.py
+++ camberleykarting/settings.py
@@ -17,7 +17,6 @@ if os.path.isfile('env.py'):
     import env
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -32,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jamieroche1987-camberley-6w4y7u3lqh.us2.codeanyapp.com', 'camberleykarting.herokuapp.com', 'localhost'] 
+ALLOWED_HOSTS = ['8000-jamieroche1987-camberley-6w4y7u3lqh.us2.codeanyapp.com',
+                 'camberleykarting.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -84,16 +84,15 @@ WSGI_APPLICATION = 'camberleykarting.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
- #     'default': {
- #         'ENGINE': 'django.db.backends.sqlite3',
- #         'NAME': BASE_DIR / 'db.sqlite3',
- #     }
- # }
-    
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
-
+}
 
 
 # Password validation
