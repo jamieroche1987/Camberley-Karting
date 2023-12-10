7087a30227a6194b01ca1ada23572bf82be238af
jamieroche1987 <>
2023-12-10 15:26:44 +0000

    Add base.html with links to css/js/bootstrap

--- karting/admin.py
+++ karting/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Services, Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """Class to view the Bookings in the admin panel"""
+    list_display = ('username', 'date_of_booking',
+                    'service_name', 'start_time', 'end_time',)
+    list_filter = ('date_of_booking',)
+    search_fields = ['username', 'service_name']
+    actions = ['conform_booking']
+
+    def confirm_booking(self, request, queryset):
+        queryset.update(confirmed=True)
+
+
+@admin.register(Services)
+class BookingServices(admin.ModelAdmin):
+    """Class to view the Services in the admin panel"""
+    list_display = ('service_name', 'session_length', 'cost')
