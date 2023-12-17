0ee8b84e8537caa42e7c66aad6f1704707569bf1
jamieroche1987 <>
2023-12-17 15:46:25 +0000

    Update HTML code on  account home page

--- camberleykarting/urls.py
+++ camberleykarting/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('karting.urls'), name='home_urls'),
+    path('', include('booking_system.urls'), name='home_urls'),
     path('accounts/', include('allauth.urls')),
 ]
--- karting/urls.py
+++ karting/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('', views.BookingsList.as_view(), name='account_home'),
     path('createbooking/', views.CreateBookingView.as_view(), name='create_booking')
