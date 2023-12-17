a47a40f595844824971e0c361eab3743aafdde0a
jamieroche1987 <>
2023-12-17 17:35:19 +0000

    Add booking.html

--- karting/forms.py
+++ karting/forms.py
@@ -1,9 +1,18 @@
 from .models import Booking
 from django import forms
+from datetime import datetime
+from django.forms.widgets import DateInput
 
 
 class BookingForm(forms.ModelForm):
-
+    """Form to create a booking for Karting"""
     class Meta:
         model = Booking
-        fields = ("date_of_booking", "service_name", "start_time",)
+        fields = ["date_of_booking", "service_name", "start_time"]
+        widgets = {'date_of_booking': DateInput(attrs={'type': 'date'}), }
+
+        labels = {
+            'date_of_booking': 'Date',
+            'service_name': 'Packages',
+            'start_time': 'Time',
+            }
--- karting/views.py
+++ karting/views.py
@@ -6,16 +6,18 @@ from .forms import BookingForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import DeleteView, CreateView, UpdateView, ListView
 
-
 class BookingsList(generic.ListView):
     model = Booking
     template_name = 'account-home.html'
     paginate_by = 25
     queryset = Booking.objects.filter(date_of_booking__gte=date.today()).order_by('date_of_booking', 'start_time')
-
-
+    
 class CreateBookingView(LoginRequiredMixin, CreateView):
     form_class = BookingForm
     template_name = 'booking/booking.html'
-    success_url = "/booking/account-home/"
+    success_url = "/accounthome/"
     model = Booking
+
+    def form_valid(self, form):
+        form.instance.username = self.request.user
+        return super().form_valid(form)
